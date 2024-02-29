007d2babe4d783dc7f9deb81c831319953de551e
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2024-02-29 08:45:39 +0800

    option　進度

--- src/app/option/option.component.ts
+++ src/app/option/option.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-option',
-  standalone: true,
-  imports: [],
-  templateUrl: './option.component.html',
-  styleUrl: './option.component.scss'
+	selector: 'app-option',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './option.component.html',
+	styleUrl: './option.component.scss'
 })
-export class OptionComponent {
 
+
+
+export class OptionComponent {
+	constructor(private el: ElementRef) { }
+	permissions_sel = {
+		title: "權限設定",
+		lists: ['權限設定', '頁面設定', '角色設定'],
+		show: false,
+		txt: "權限設定",
+		on: true
+	}
+	ngOnInit(): void {
+
+	}
+	option_sel(s: string) {
+		switch (s) {
+			case 'permissions':
+				this.permissions_sel.show = !this.permissions_sel.show
+				break;
+
+			default:
+				break;
+		}
+		console.log('this', s);
+
+	}
+	option_item(s: string) {
+		this.permissions_sel.txt = s
+	}
+
+	alert_close(s: string) {
+
+	}
 }
