257d1db93ebb506faa0176bbbd27c9db4f778e82
yji35j42012 <yji35j42012@gmail.com>
2024-03-11 14:33:02 +0800

    1.scroll bar 問題 scss修正 請直接把scss蓋檔 2.模糊問題 這兩個 canvas幫加上width以及height <canvas id="myChart" class="chart" width="1440" height="500"></canvas>  <canvas id="energy_Chart" class="chart" width="1440" height="500"></canvas>

--- src/app/e-kpi/e-kpi.component.ts
+++ src/app/e-kpi/e-kpi.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModelCalendarComponent } from '../model-calendar/model-calendar.component';
 
 @Component({
   selector: 'app-e-kpi',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ModelCalendarComponent],
   templateUrl: './e-kpi.component.html',
-  styleUrl: './e-kpi.component.scss'
+  styleUrl: './e-kpi.component.scss',
 })
 export class EKpiComponent {
+  kpi_filter = {
+    start: {
+      isShow: true,
+      year: 2024,
+      month: 2,
+      day: 11,
+      hour: 10,
+      min: 11,
+    },
+    end: {
+      isShow: false,
+      year: 2024,
+      month: 3,
+      day: 11,
+      hour: 10,
+      min: 11,
+    },
+  };
+  calendar = {
+    isShow: false,
+    year: 2024,
+    month: 2,
+    day: 11,
+    hour: 10,
+    min: 11,
+  };
 
+  dateHanler(s: string) {
+    var item: any;
+    if (s == 'start') {
+      this.kpi_filter.end.isShow = false;
+      this.kpi_filter.start.isShow = !this.kpi_filter.start.isShow;
+      item = this.kpi_filter.start;
+    } else if (s == 'end') {
+      this.kpi_filter.start.isShow = false;
+      this.kpi_filter.end.isShow = !this.kpi_filter.end.isShow;
+      item = this.kpi_filter.end;
+    }
+    this.calendar.year = item.year;
+    this.calendar.month = item.month;
+    this.calendar.day = item.day;
+    this.calendar.hour = item.hour;
+    this.calendar.min = item.min;
+  }
 }
--- src/app/model-calendar/model-calendar.component.ts
+++ src/app/model-calendar/model-calendar.component.ts
@@ -1,59 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-	selector: 'app-model-calendar',
-	standalone: true,
-	imports: [CommonModule],
-	templateUrl: './model-calendar.component.html',
-	styleUrl: './model-calendar.component.scss'
+  selector: 'app-model-calendar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './model-calendar.component.html',
+  styleUrl: './model-calendar.component.scss',
 })
 export class ModelCalendarComponent {
-	year: number = 2024; // 默认年份为 2024
-	month: number = 3; // 默认月份为 3
-	dates: any[] = [];
-	generateRandomDates(year: number, month: number) {
-		const firstDayOfMonth = new Date(year, month - 1, 1).getDay(); // 获取每月的第一天是星期几（0表示星期日，1表示星期一，以此类推）
-		const daysInMonth = new Date(year, month, 0).getDate(); // 获取该月份的天数
-		this.dates = [];
-		let week = [];
+  @Input() calendar_content: any = {};
+  // console.log('calendar_content',calendar_content)
+  year: number = 2024; // 默认年份为 2024
+  month: number = 3; // 默认月份为 3
+  dates: any[] = [];
+  generateRandomDates(year: number, month: number) {
+    const firstDayOfMonth = new Date(year, month - 1, 1).getDay(); // 获取每月的第一天是星期几（0表示星期日，1表示星期一，以此类推）
+    const daysInMonth = new Date(year, month, 0).getDate(); // 获取该月份的天数
+    this.dates = [];
+    let week = [];
+    // 添加每月第一天之前的日期
+    for (let i = 0; i < firstDayOfMonth; i++) {
+      week.push(null);
+    }
 
-		// 添加每月第一天之前的日期
-		for (let i = 0; i < firstDayOfMonth; i++) {
-			week.push(null);
-		}
+    // 添加该月份的所有日期
+    for (let i = 1; i <= daysInMonth; i++) {
+      const date = { year: year, month: month, day: i };
+      week.push(i);
+      if (week.length === 7 || i === daysInMonth) {
+        this.dates.push(week);
+        week = [];
+      }
+    }
+  }
 
-		// 添加该月份的所有日期
-		for (let i = 1; i <= daysInMonth; i++) {
-			const date = { "year": year, "month": month, "day": i };
-			week.push(i);
-			if (week.length === 7 || i === daysInMonth) {
-				this.dates.push(week);
-				week = [];
-			}
-		}
-	}
+  prevMonth() {
+    this.month--;
+    if (this.month === 0) {
+      this.month = 12;
+      this.year--;
+    }
+    this.generateRandomDates(this.year, this.month);
+  }
 
-	prevMonth() {
-		this.month--;
-		if (this.month === 0) {
-			this.month = 12;
-			this.year--;
-		}
-		this.generateRandomDates(this.year, this.month);
-	}
-
-	nextMonth() {
-		this.month++;
-		if (this.month === 13) {
-			this.month = 1;
-			this.year++;
-		}
-		this.generateRandomDates(this.year, this.month);
-	}
-	ngOnInit(): void {
-		this.generateRandomDates(this.year, this.month)
-	}
-
-
+  nextMonth() {
+    this.month++;
+    if (this.month === 13) {
+      this.month = 1;
+      this.year++;
+    }
+    this.generateRandomDates(this.year, this.month);
+  }
+  ngOnInit(): void {
+    console.log('calendar_content', this.calendar_content);
+    this.year = this.calendar_content.year;
+    this.month = this.calendar_content.month;
+    this.generateRandomDates(this.year, this.month);
+  }
 }
