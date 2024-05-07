8da1872522c5ea69cdca4ba7438261368a278802
yji35j42012 <yji35j42012@gmail.com>
2024-05-07 13:47:15 +0800

    Equipment Center 點擊選單外的地方應該自動收起

--- src/app/equipment-center/equipment-center.component.ts
+++ src/app/equipment-center/equipment-center.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModelSelectComponent } from '../model-select/model-select.component';
 import { ModelCalendarComponent } from '../model-calendar/model-calendar.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
 	selector: 'app-equipment-center',
 	standalone: true,
-	imports: [CommonModule, ModelSelectComponent, ModelCalendarComponent],
+	imports: [
+		CommonModule,
+		ModelSelectComponent,
+		ModelCalendarComponent,
+		FormsModule,
+	],
 	templateUrl: './equipment-center.component.html',
-	styleUrl: './equipment-center.component.scss'
+	styleUrl: './equipment-center.component.scss',
 })
 export class EquipmentCenterComponent {
 	line_sel = {
@@ -18,12 +24,14 @@ export class EquipmentCenterComponent {
 		isShow: false,
 		msg: 'Line',
 	};
-	selShow(s: string) {
+	selShow (s: string) {
+		this.equipment_filter.end.isShow = false;
+		this.equipment_filter.start.isShow = false;
 		if (s == 'line_sel') {
 			this.line_sel.isShow = !this.line_sel.isShow;
 		}
 	}
-	selHandler(s: any) {
+	selHandler (s: any) {
 		if (s.name == 'line_sel') {
 			this.line_sel.title = s.title;
 			this.line_sel.isShow = false;
@@ -56,7 +64,8 @@ export class EquipmentCenterComponent {
 		hour: 10,
 		min: 11,
 	};
-	dateHanler(s: string) {
+	dateHanler (s: string) {
+		this.line_sel.isShow = false;
 		var item: any;
 		if (s == 'start') {
 			this.equipment_filter.end.isShow = false;
@@ -74,7 +83,7 @@ export class EquipmentCenterComponent {
 		this.calendar.hour = item.hour;
 		this.calendar.min = item.min;
 	}
-	calerdarReturnHandler(obj: any) {
+	calerdarReturnHandler (obj: any) {
 		var item: any;
 		if (this.calendar.state == 'start') {
 			item = this.equipment_filter.start;
@@ -91,209 +100,226 @@ export class EquipmentCenterComponent {
 	equipment_data = [
 		{
 			line: 's1',
-			machine: "Printer-NS-0001(DEK-1)",
+			machine: 'Printer-NS-0001(DEK-1)',
 			isWarning: true,
 			plan: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
 			],
 			result: [
 				{
 					value: 20,
 					color: 'green',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 35,
 					color: 'red',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 45,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
-			]
+			],
 		},
 		{
 			line: 's2',
-			machine: "Printer-NS-0001(DEK-2)",
+			machine: 'Printer-NS-0001(DEK-2)',
 			isWarning: false,
 			plan: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
 			],
 			result: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
-			]
+			],
 		},
 		{
 			line: 's2',
-			machine: "Printer-NS-0001(DEK-2)",
+			machine: 'Printer-NS-0001(DEK-2)',
 			isWarning: false,
 			plan: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
 			],
 			result: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
-			]
+			],
 		},
 		{
 			line: 's3',
-			machine: "Printer-NS-0001(DEK-2)",
+			machine: 'Printer-NS-0001(DEK-2)',
 			isWarning: false,
 			plan: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
 			],
 			result: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
-			]
+			],
 		},
 		{
 			line: 's4',
-			machine: "Printer-NS-0001(DEK-2)",
+			machine: 'Printer-NS-0001(DEK-2)',
 			isWarning: false,
 			plan: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
 			],
 			result: [
 				{
 					value: 10,
 					color: 'red',
-					msg: 'EQ_WAIT'
+					msg: 'EQ_WAIT',
 				},
 				{
 					value: 20,
 					color: 'green',
-					msg: "PD_RUN"
+					msg: 'PD_RUN',
 				},
 				{
 					value: 30,
 					color: 'orange',
-					msg: "ENG"
+					msg: 'ENG',
 				},
-			]
+			],
 		},
-	]
+	];
 
+	equipment_detail = {
+		isShow: false,
+	};
+	equipmentHandler () {
+		this.equipment_detail.isShow = true;
+	}
+	alert_close () {
+		this.equipment_detail.isShow = false;
+	}
 
-	equipment_detail={
-		isShow:false
+	isHostListener: Boolean = false;
+	mouseevent (b: Boolean) {
+		this.isHostListener = b;
 	}
-	equipmentHandler(){
-		this.equipment_detail.isShow=true
-	}
-	alert_close(){
-		this.equipment_detail.isShow=false
+	@HostListener('document:click', ['$event'])
+	onClick (event: MouseEvent) {
+		if (this.isHostListener) return;
+		if (this.line_sel.isShow) {
+			this.line_sel.isShow = false;
+		}
+		if (this.equipment_filter.end.isShow) {
+			this.equipment_filter.end.isShow = false;
+		}
+		if (this.equipment_filter.start.isShow) {
+			this.equipment_filter.start.isShow = false;
+		}
 	}
 }
